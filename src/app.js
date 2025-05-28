612b4f61150d4a1494231e9bb43800901de861f1
Sebastian <133646707+Hizuis@users.noreply.github.com>
2025-05-28 23:20:10 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+  let quien = ['El perro', 'Mi abuela', 'El cartero', 'Mi gato'];
+  let accion = ['comió', 'orinó sobre', 'aplastó', 'rompió'];
+  let que = ['mi tarea', 'mi teléfono', 'el coche'];
+  let cuando = [
+    'antes de clase',
+    'mientras dormía',
+    'mientras hacía ejercicio',
+    'durante el almuerzo',
+    'mientras rezaba'
+  ];
 
+  function generarExcusa() {
+    let quienAleatorio = quien[Math.floor(Math.random() * quien.length)];
+    let accionAleatoria = accion[Math.floor(Math.random() * accion.length)];
+    let queAleatorio = que[Math.floor(Math.random() * que.length)];
+    let cuandoAleatorio = cuando[Math.floor(Math.random() * cuando.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    return quienAleatorio + " " + accionAleatoria + " " + queAleatorio + " " + cuandoAleatorio + ".";
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function mostrarExcusa() {
+    document.getElementById("excuse").innerHTML = generarExcusa();
+  }
+  
+  document.getElementById("botonExcusa").addEventListener("click", mostrarExcusa);
+  
 };
